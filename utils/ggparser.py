1f9cf6caee29ff033df78b182bd6c7da57dd7d34
Gary James <garyjames@users.noreply.github.com>
2024-11-10 19:27:42 -0600

    load_symbol

--- utils/ggparser.py
+++
@@ -1,9 +0,0 @@
-from IEXTools import Parser, messages
-
-class TradeParser(Parser):
-    def __init__(self, filepath):
-        self.filter_tradereport = [messages.TradeReport]
-        super().__init__(filepath)
-
-    def __next__(self):
-        return self.get_next_message(self.filter_tradereport)
